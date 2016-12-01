614cd632758f9c776ce146420ef4e6ec5b29160a
RaoulMeyer <raouljmeyer@gmail.com>
2016-12-01 16:31:42 +0100

    Reformated code and small fixes

--- flappy/server.js
+++ flappy/server.js
@@ -2,25 +2,26 @@ var http = require('http');
 
 var WebSocketServer = require('websocket').server;
 
-var server = http.createServer(function(request, response) {});
+var server = http.createServer(function (request, response) {
+});
 
-var clients = [ ];
+var clients = [];
 
-var obstacles = [ ];
+var obstacles = [];
 
-for(var i = 0; i < 100; i++) {
-  obstacles.push(Math.floor(Math.random()*300));
+for (var i = 0; i < 100; i++) {
+    obstacles.push(Math.floor(Math.random() * 300));
 }
 
 function Message(type) {
-  this.type = type;
-  this.id = 0;
-  this.y;
-  this.obstacles = obstacles;
+    this.type = type;
+    this.id = 0;
+    this.y = null;
+    this.obstacles = obstacles;
 }
 
 
-server.listen(9999, function() {
+server.listen(9999, function () {
     console.log((new Date()) + " Server is listening on port " + 9999);
 });
 
@@ -28,7 +29,7 @@ var wsServer = new WebSocketServer({
     httpServer: server
 });
 
-wsServer.on('request', function(request) {
+wsServer.on('request', function (request) {
     console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
 
     var connection = request.accept(null, request.origin);
@@ -38,20 +39,20 @@ wsServer.on('request', function(request) {
     console.log((new Date()) + ' Connection accepted.');
 
     // user sent some message
-    connection.on('message', function(message) {
+    connection.on('message', function (message) {
         if (message.type === 'utf8') {
-          var data = JSON.parse(message.utf8Data);
-          if(data.type == "getObstacles") {
-            console.log(JSON.stringify(new Message("obstacles")));
-            connection.sendUTF(JSON.stringify(new Message("obstacles")));
-          } else {
-            // broadcast message to all connected clients
-            for (var i=0; i < clients.length; i++) {
-                if(clients[i] != connection && clients[i] != null) {
-                  clients[i].sendUTF(message.utf8Data);
+            var data = JSON.parse(message.utf8Data);
+            if (data.type == "getObstacles") {
+                console.log(JSON.stringify(new Message("obstacles")));
+                connection.sendUTF(JSON.stringify(new Message("obstacles")));
+            } else {
+                // broadcast message to all connected clients
+                for (var i = 0; i < clients.length; i++) {
+                    if (clients[i] != connection && clients[i] != null) {
+                        clients[i].sendUTF(message.utf8Data);
+                    }
                 }
             }
-          }
         }
     });
 });
