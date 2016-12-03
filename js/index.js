9d0668bb4cdebd0347d8ea45797414497c2c1cf6
RaoulMeyer <raouljmeyer@gmail.com>
2016-12-03 23:10:20 +0100

    Added a basic menu and some nice transitions, also refactored and cleaned up some

--- js/index.js
+++ js/index.js
@@ -1,13 +1,35 @@
 $(function() {
-    var duration = 12000;
+    var body = $('body');
+    if (body.hasClass('intro')) {
+        var duration = 12000;
 
-    $("#title-content").animate({
-        top: '-100%'
-    }, duration, 'linear', function() {
-        $('#menu').fadeIn();
+        $("#title-content").animate({
+            top: '-100%'
+        }, duration, 'linear', function () {
+            $('#start-menu').fadeIn();
+        });
+
+        body.animate({
+            'background-size': '120%'
+        }, duration, 'linear');
+    }
+
+    if (body.hasClass('menu')) {
+        $('#menu').show();
+    }
+
+    function transition(href) {
+    }
+
+    $("#transition").fadeOut(1000);
+    $('a').click(function (e) {
+        e.preventDefault();
+
+        var href = $(this).attr('href');
+        $("#transition").fadeIn(1000);
+        setTimeout(function() {window.location = href}, 1200);
+
+        return false;
     });
 
-    $('body').animate({
-        'background-size': '120%'
-    }, duration, 'linear')
 });
