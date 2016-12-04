1d27e9df5166d72d2a808e32794af0a062dce6c4
RaoulMeyer <raouljmeyer@gmail.com>
2016-12-04 22:52:51 +0100

    Added loading screen that caches all large files

--- js/index.js
+++ js/index.js
@@ -22,6 +22,49 @@ $(function() {
         })
     }
 
+    if (body.hasClass('loading')) {
+        var fetched = 0;
+
+        var urls = [
+            './audio/flappy.mp3',
+            './audio/intro.mp3',
+            './audio/menu.mp3',
+            './audio/pacman.mp3',
+            './audio/spaceinvaders.mp3',
+            './audio/victory.mp3',
+            './img/falcon.png',
+            './img/r2d2.png',
+            './img/stars.png',
+            './img/xwing.png',
+            './flappy/img/background.png',
+            './flappy/img/ship.png',
+            './flappy/img/wall.png',
+            './pacman/sound/die.mp3',
+            './pacman/sound/eat-fruit.mp3',
+            './pacman/sound/eat-ghost.mp3',
+            './pacman/sound/eat-pill.mp3',
+            './pacman/sound/eating.mp3',
+            './pacman/sound/extra-life.mp3',
+            './pacman/sound/ghost-eaten.mp3',
+            './pacman/sound/ready.mp3',
+            './pacman/sound/siren.mp3',
+            './pacman/sound/waza.mp3'
+        ];
+
+        for (var i = 0; i < urls.length; i++) {
+            $.get(urls[i]).done(function () {
+                fetched++;
+                $('#loading-inner').animate({
+                    'width': 100 - 100 * fetched / urls.length + '%'
+                }, 100, null, function() {
+                    if (fetched >= urls.length) {
+                        setTimeout(function() { $('#index-link').click(); }, 1000);
+                    }
+                });
+            });
+        }
+    }
+
     $('#transition').fadeOut(1000);
     $('a').click(function (e) {
         e.preventDefault();
