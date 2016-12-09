7b2a680c712241596f0040d8dd30b5a823d46c88
RaoulMeyer <raouljmeyer@gmail.com>
2016-12-09 01:03:35 +0100

    Fixed undefined js functions

--- js/index.js
+++ js/index.js
@@ -81,24 +81,25 @@ $(function() {
         return false;
     });
 
-    function won(part) {
-        var params = getUrlVars();
-
-        if (params.hasOwnProperty('token')) {
-            $.get("/won.php", { token: params['token'], part: part } );
-        }
-    }
-
-    function getUrlVars()
-    {
-        var vars = [], hash;
-        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-        for(var i = 0; i < hashes.length; i++)
-        {
-            hash = hashes[i].split('=');
-            vars.push(hash[0]);
-            vars[hash[0]] = hash[1];
-        }
-        return vars;
-    }
 });
+
+function won(part) {
+    var params = getUrlVars();
+
+    if (params.hasOwnProperty('token')) {
+        $.get("/won.php", { token: params['token'], part: part } );
+    }
+}
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
